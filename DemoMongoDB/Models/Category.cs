b33aabbf153f1421d9473315be55e46919b30925
THIEN PHAM DINH <pdthien@fresher.local>
2020-01-07 17:28:20 +0700

    first commit

---
+++ DemoMongoDB/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMongoDB.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("item")]
+        public string Item { get; set; }
+
+        [BsonElement("qty")]
+        public double Qty { get; set; }
+    }
+}
---
+++ DemoMongoDB/Services/CategoryService.cs
@@ -0,0 +1,54 @@
+﻿using DemoMongoDB.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMongoDB.Services
+{
+    public class CategoryService
+    {
+        private readonly IMongoCollection<Category> _categories;
+
+        public CategoryService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("ExpenseStoreMongoDb"));
+            var database = client.GetDatabase("demo");
+            _categories = database.GetCollection<Category>("categories");
+        }
+
+        public List<Category> Get()
+        {
+            return _categories.Find(new BsonDocument()).ToList();
+        }
+
+        public Category Get(string id)
+        {
+            return _categories.Find<Category>(category => category.Id == id).FirstOrDefault();
+        }
+
+        public Category Create(Category category)
+        {
+            _categories.InsertOne(category);
+            return category;
+        }
+
+        public void Update(string id, Category bookIn)
+        {
+            _categories.ReplaceOne(category => category.Id == id, bookIn);
+        }
+
+        public void Remove(Category bookIn)
+        {
+            _categories.DeleteOne(category => category.Id == bookIn.Id);
+        }
+
+        public void Remove(string id)
+        {
+            _categories.DeleteOne(category => category.Id == id);
+        }
+    }
+}
